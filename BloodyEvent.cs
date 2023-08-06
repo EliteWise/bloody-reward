ae92fb4c9e1c2a12c25e5e264c2f0919f4b04d6b
Florent <eliteman.fm@gmail.com>
2023-08-06 11:07:44 +0200

    Init Plugin

---
+++ BloodyEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenMod.API.Eventing;
+using OpenMod.Unturned.Events;
+using OpenMod.Unturned.Players;
+using OpenMod.Unturned.Players.Stats.Events;
+using OpenMod.Unturned.Zombies.Events;
+using SDG.Unturned;
+
+namespace MyOpenModPlugin
+{
+    public class BloodyEvent : IEventListener<UnturnedPlayerBleedingUpdatedEvent>, IEventListener<UnturnedZombieDeadEvent>
+    {
+
+        private bool isPlayerBleeding = false;
+
+        public Task HandleEventAsync(object? sender, UnturnedPlayerBleedingUpdatedEvent e)
+        {
+            isPlayerBleeding = e.IsBleeding;
+            return Task.CompletedTask;
+
+        }
+
+        public Task HandleEventAsync(object? sender, UnturnedZombieDeadEvent e)
+        {
+            //EffectManager.sendUIEffect()
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CommandEcho.cs
@@ -0,0 +1,37 @@
+﻿using OpenMod.Core.Commands;
+using System;
+using System.Threading.Tasks;
+using OpenMod.Unturned.Players;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using Command = OpenMod.Core.Commands.Command;
+
+namespace MyOpenModPlugin
+{
+    [Command("awesome")]
+    [CommandAlias("awsm")]
+    [CommandAlias("aw")]
+    [CommandDescription("My awesome command")]
+    public class CommandAwesome : Command
+    {
+
+        public CommandAwesome(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        protected async override Task OnExecuteAsync()
+        {
+
+            var player = (UnturnedUser)base.Context.Actor;
+            var playerTransform = ((UnturnedPlayer)player.Player).Player.transform;
+            var position = playerTransform.position + playerTransform.forward * 2;
+            var entityId = "1"; // Change this to the ID of the entity you want to spawn
+
+            Item item = new Item(14, true);
+            player.Player.Player.inventory.forceAddItem(item, true);
+
+            await PrintAsync("Item is here!");
+
+        }
+    }
+}
