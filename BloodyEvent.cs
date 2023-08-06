e5a162220cce5999c57a603a3e0a999791ab3b63
Florent <eliteman.fm@gmail.com>
2023-08-06 23:22:24 +0200

    Implement item spawning + change event

--- BloodyEvent.cs
+++ BloodyEvent.cs
@@ -9,10 +9,14 @@ using OpenMod.Unturned.Players;
 using OpenMod.Unturned.Players.Stats.Events;
 using OpenMod.Unturned.Zombies.Events;
 using SDG.Unturned;
+using OpenMod.Unturned.Users;
+using OpenMod.Extensions.Games.Abstractions.Entities;
+using System.Numerics;
+using OpenMod.Unturned.Entities;
 
 namespace MyOpenModPlugin
 {
-    public class BloodyEvent : IEventListener<UnturnedPlayerBleedingUpdatedEvent>, IEventListener<UnturnedZombieDeadEvent>
+    public class BloodyEvent : IEventListener<UnturnedPlayerBleedingUpdatedEvent>, IEventListener<UnturnedZombieDyingEvent>
     {
 
         private bool isPlayerBleeding = false;
@@ -24,10 +28,19 @@ namespace MyOpenModPlugin
 
         }
 
-        public Task HandleEventAsync(object? sender, UnturnedZombieDeadEvent e)
+        public async Task HandleEventAsync(object? sender, UnturnedZombieDyingEvent e)
         {
+            if (e.Instigator != null)
+            {
+                UnturnedPlayer player = e.Instigator;
+                String speciality = e.Zombie.Zombie.speciality.ToString();
+                await player.PrintMessageAsync($"Data: {player.Player.name} - {speciality}");
+                Item item = new Item(95, true);
+                ItemManager.dropItem(item, player.Player.transform.position, true, true, false);
+            }
+
             //EffectManager.sendUIEffect()
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
     }
 }
--- CommandEcho.cs
+++ CommandEcho.cs
@@ -22,14 +22,6 @@ namespace MyOpenModPlugin
         protected async override Task OnExecuteAsync()
         {
 
-            var player = (UnturnedUser)base.Context.Actor;
-            var playerTransform = ((UnturnedPlayer)player.Player).Player.transform;
-            var position = playerTransform.position + playerTransform.forward * 2;
-            var entityId = "1"; // Change this to the ID of the entity you want to spawn
-
-            Item item = new Item(14, true);
-            player.Player.Player.inventory.forceAddItem(item, true);
-
             await PrintAsync("Item is here!");
 
         }
