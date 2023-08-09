d4ffd2a506fb4d5653db0ce45e76b4398876240c
Florent <eliteman.fm@gmail.com>
2023-08-09 23:30:07 +0200

    [Feature] Add specific drops for each zombie type - #1

--- BloodyEvent.cs
+++ BloodyEvent.cs
@@ -20,6 +20,18 @@ namespace MyOpenModPlugin
     {
 
         private bool isPlayerBleeding = false;
+        private Dictionary<EZombieSpeciality, ushort> zombieItemIdBySpeciality = new Dictionary<EZombieSpeciality, ushort>
+        {
+            { EZombieSpeciality.NORMAL, 95 },
+            { EZombieSpeciality.BURNER, 15 },
+            { EZombieSpeciality.FLANKER_STALK, 389 },
+            { EZombieSpeciality.CRAWLER, 391 },
+            { EZombieSpeciality.ACID, 392 },
+            { EZombieSpeciality.SPRINTER, 393 },
+            { EZombieSpeciality.SPIRIT, 395 }
+        };
+
+
 
         public Task HandleEventAsync(object? sender, UnturnedPlayerBleedingUpdatedEvent e)
         {
@@ -35,8 +47,12 @@ namespace MyOpenModPlugin
                 UnturnedPlayer player = e.Instigator;
                 String speciality = e.Zombie.Zombie.speciality.ToString();
                 await player.PrintMessageAsync($"Data: {player.Player.name} - {speciality}");
-                Item item = new Item(95, true);
-                ItemManager.dropItem(item, player.Player.transform.position, true, true, false);
+                if(Enum.TryParse(e.Zombie.Zombie.name, true, out EZombieSpeciality zombieSpeciality))
+                {
+                    ushort itemId = zombieItemIdBySpeciality[zombieSpeciality];
+                    Item item = new Item(itemId, true);
+                    ItemManager.dropItem(item, player.Player.transform.position, true, true, false);
+                }
             }
 
             //EffectManager.sendUIEffect()
